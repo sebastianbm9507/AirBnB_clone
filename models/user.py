d4bd10a63c30cd5d15b34632e7a975d25c110cc6
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-06-30 10:30:44 -0500

    Create User class

---
+++ models/user.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Import Modules """
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """ Class User """
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
